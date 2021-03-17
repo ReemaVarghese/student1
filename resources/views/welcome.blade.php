3dcfdab829344994c52f68a044653c814c9b030a
ReemaVarghese <mca540@rajagiri.edu>
2021-03-17 12:45:29 +0530

    StudentAppNew

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,24 @@
     <title>Student</title>
 </head>
 <body>
+<nav class="navbar navbar-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="primary" class="bi bi-briefcase-fill" viewBox="0 0 16 16">
+  <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z"/>
+</svg>
+        <a class="nav-link active" aria-current="page" href="\student">Home</a>
+        <a class="nav-link" href="\contact">Contact</a>
+        <a class="nav-link" href="\faculty">Faculty</a>
+      </div>
+    </div>
+  </div>
+</nav>
 <div class="container">
 <div class="row">
 </div>
@@ -37,6 +55,6 @@
 </tr></table>
 </div></div>
 </div>
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/student', function () {
     return view('welcome');
+});
+
+Route::get('/contact',function() {
+    return view('contact');
+});
+
+Route::get('/faculty',function() {
+    return view('faculty');
 });
